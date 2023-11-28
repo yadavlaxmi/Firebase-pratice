69161fa68bb456c3483b8a6d7bcafd0dcee4adf4
navgurukul <laxmiyadav21@navgurukul.org>
2023-11-28 11:10:16 +0530

    write code of real time database

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-// import {getDatabase,ref,set}from "firebase/database"
+import {getDatabase,ref,set}from "firebase/database"
 import './App.css';
 import {getAuth,createUserWithEmailAndPassword} from "firebase/auth"
 import {app} from "./Firebase/Firebase"
-// const db=getDatabase(app)
+const db=getDatabase(app)
 const auth=getAuth(app);
 function App() {
   const signupUser=()=>{
@@ -11,16 +11,18 @@ function App() {
     "laxmi@123"
     ).then((value)=>console.log(value));
   };
-  // const putData=()=>{
-  //   set(ref(db,"users/laxmi"),{
-  //     id:1,
-  //     name:"laxmi",
-  //     age:"17",
-  //   })
-  // }
+  const putData=()=>{
+    set(ref(db,"users/laxmi"),{
+      id:1,
+      name:"laxmi",
+      age:"17",
+    })
+  }
   return (
     <div className="App">
       <h1>Firebase aap</h1>
+      <button onClick={putData}>put data</button>
+
       <button onClick={signupUser}>create data</button>
     </div>
   );
