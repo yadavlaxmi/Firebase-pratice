1b5fe22c78cb1b03ce162eb40bedf2c046f37ba1
navgurukul <laxmiyadav21@navgurukul.org>
2023-11-29 15:07:45 +0530

    realtime database firebase

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-import {useState} from "react"
+import { useState } from "react";
+import { useFirebase } from "./Context/Firebase";
 
-import {useFirebase} from "./Context/Firebase"
-const App=()=>{
-  const firebase=useFirebase();
-  const[email,setEmail]=useState("")
-  const[password,setPassword]=useState("")
-  console.log("Firebase",firebase)
-  const putDataNew=()=>{
-    firebase.putData("grandFather/Father/child",{id :1,name:"laxmi",age:19})
-  }
-  return(
-  <>
-  <h1>firebase</h1>
-  <input onChange={e=>setEmail(e.target.value)}
-  value={email}
-  type="email"
-    placeholder="enter email "/>
-  <input onChange={e=>setPassword(e.target.value)}
-  value={password}
-  type="passsword" 
-  placeholder="enter password "/>
-  <button onClick={()=>{
-    firebase.signupUserWithEmailAndPassword(email,password)
-    firebase.putData("users/"+ "laxmi",{email,password})
-    }}>Signup</button>
-    <button OnClick={putDataNew}>Trigger</button>
+const App = () => {
+  const firebase = useFirebase();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  </>
-  )
-}
-export default App
+  const putDataNew = () => {
+    firebase.putData("grandFather/Father/child", { id: 1, name: "laxmi", age: 19 });
+  };
+
+  const handleSignup = async () => {
+    try {
+      await firebase.signupUserWithEmailAndPassword(email, password);
+      await firebase.putData("users/laxmi", { email, password });
+    } catch (error) {
+      console.error("Error signing up:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Firebase</h1>
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        type="email"
+        placeholder="Enter email"
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        type="password"
+        placeholder="Enter password"
+      />
+      <button onClick={handleSignup}>Signup</button>
+      <button onClick={putDataNew}>Trigger</button>
+    </>
+  );
+};
+
+export default App;
--- src/Context/Firebase.js
+++ src/Context/Firebase.js
@@ -1,7 +1,7 @@
-import {createContext, useContext} from "react"
+import {useEffect,useState,createContext, useContext} from "react"
 import {initializeApp} from "firebase/app"
 import {getAuth,createUserWithEmailAndPassword} from "firebase/auth"
-import {getDatabase,set,ref} from "firebase/database"
+import {getDatabase,set,ref,get,child,onValue} from "firebase/database"
 const firebaseConfig = {
     apiKey: "AIzaSyCbmybfGOG5kv3RLD3oH3z6hnsctwrKOjA",
     authDomain: "my-project-5e792.firebaseapp.com",
@@ -20,11 +20,25 @@ export const useFirebase=()=>useContext(FirebaseContext)
 
 
 export const FirebaseProvider=(props)=>{
+    const [name,setName]=useState("")
     const signupUserWithEmailAndPassword=(email,password)=>{
         return createUserWithEmailAndPassword(firebaseAuth,email,password)}
-    const putData=(key,data)=>set(ref(database,key),data)
-        return(
+    const putData=(key,data)=>set(ref(database,key),data);
+    // get(child(ref(database),"grandfather/father/child")).then(snapshot=>{
+    //     console.log(snapshot.val())
+    // })
+
+    onValue(ref(database,"grandfather/father/child"),(snapshot)=>console.log(snapshot.val()));
+        setName(snapshot.val().name)
+    
+    useEffect(()=>{
+        onValue(ref(database,"grandfather/father/child"),(snapshot)=>console.log(snapshot.val()));
+        setName(snapshot.val().name)
+    
+    },[])
+    return(
         <FirebaseContext.Provider value={{signupUserWithEmailAndPassword,putData}}>
+           <h3>name is{name} </h3>
             {props.children}
         </FirebaseContext.Provider>
     )
